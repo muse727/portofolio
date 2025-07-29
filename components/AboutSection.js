116b464c3b3ff0e60355d8e59c45312abd2e9aa6
muse727 <samsulmustofa000@gmail.com>
2025-07-29 09:02:00 +0700

    membuat section kedua dengan menambahkan aboutme dan mempercantik animasi

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -1,5 +1,7 @@
 import styles from './AboutSection.module.css';
 import ProfileCard from './ProfileCard'; // Import komponen yang baru kita buat
+import ShinyText from './ShinyText';
+import ScrollReveal from './ScrollReveal';
 
 export default function AboutSection() {
   return (
@@ -9,12 +11,12 @@ export default function AboutSection() {
         
         {/* === BAGIAN KIRI (Teks) === */}
         <div className={styles.leftColumn}>
-          <h2 className={styles.title}>About Me</h2>
-          <p className={styles.description}>
+          <ShinyText text="About Me" className={styles.title} />
+          <ScrollReveal textClassName={styles.description}>
             I'm a freelance Webdesigner and Photographer based in Jakarta, Indonesia. 
             I specialize in creating modern, responsive, and user-friendly websites. 
             My passion lies in blending clean design with fluid functionality to deliver an exceptional digital experience.
-          </p>
+          </ScrollReveal>
           <p className={styles.description}>
             Let's create something amazing together!
           </p>
